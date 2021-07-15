9d577e942bce202ed78b43f7e42a0e11f32bccc3
ihaise <86022041+ihaise@users.noreply.github.com>
2021-07-16 00:07:34 +0545

    Create infinite loop hscope.py

---
+++ infinite loop hscope.py
@@ -0,0 +1,58 @@
+def demo():
+	a=input("Enter your name")
+	print(f"jhhjfhfjhjhj {a}")
+	saiki = input(""" "enter Your Zodiac Sign" """).lower()
+	
+	if saiki == 'leo':
+	  print("Leo is 1000")
+	
+	elif saiki == 'aries': 
+	   print("aries is 2000")
+	
+	elif saiki == 'gemini':
+	  print("gemini is 4000")
+	
+	elif saiki == 'cancer':
+	   print("cancer is 5000")
+	
+	elif saiki == 'virgo':
+	  print("virgo is 6000")
+	
+	elif saiki == 'sagittarius':
+	   print("sagittarius is 7000")
+	
+	elif saiki == 'aquaris':
+	  print("aquaris is 8000")
+	
+	elif saiki == 'pisces':
+	   print("pisces is 9000")
+	
+	   
+	
+	elif saiki == 'taurus':
+	   print("taurus is 11000")
+	
+	elif saiki == 'capricorn':
+	  print("capricorn is 12000")
+	
+	elif saiki == 'pisces':
+	   print("pisces is 13000")
+		
+	elif saiki=="rom":
+		print("hey rom guy")
+	
+	else:
+	  print(""" *Your Ìnput Was Wrong. Pls enter your zodiac sign """)
+	  
+	
+	  print(""" "This Program Was developed By me aka Light Yagami" """)
+	  demo()
+	
+	
+	
+	
+demo()
+demo()
+demo()
+demo()
+	
